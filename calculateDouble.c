ac500a96ca0ca0cc7cdae3ccee817bf8c3e8fcc6
justin1228 <didls@hanyang.ac.kr>
2020-04-29 00:11:24 +0900

    addPercentageSymbol

--- calculateDouble.c
+++ calculateDouble.c
@@ -4,9 +4,9 @@ int main(){
     double num1, num2;
 
     printf("num1: ");
-    scanf("lf", &num1);
+    scanf("%lf", &num1);
     printf("num2: ");
-    scanf("lf", &num2);
+    scanf("%lf", &num2);
 
 
     printf("plus: %lf minus: %lf multifly: %lf divide: %lf", num1+num2, num1-num2, num1*num2, num1/num2);
