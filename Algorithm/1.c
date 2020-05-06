0f9efd22e2fb4d9ea64438231556205815ddfd06
justin1228 <didls1228@gmail.com>
2020-05-07 01:57:37 +0900

    add

--- Algorithm/1.c
+++ Algorithm/1.c
@@ -1,26 +1,24 @@
 #include <stdio.h>
 
 
-void Swap(int a, int b, int tmp){
-   tmp = a;a =b; b = tmp;
-}
-
-
 int main(){
-    int arr[]= {1,10,5,8,7,6,4,3,2,9};
     int i,j,min,index, temp;
+    int arr[10]= {1,10,5,8,7,6,4,3,2,9};
+    
 
     for (i=0; i<10;i++){
-        min = arr[i];
+        min = 999;
         for (j=i; j<10 ; j++){
-            if(arr[j]< min){
+            if(min > arr[j]){
                 min = arr[j];
                 index = j;
                 
             }
             
         }
-        Swap(arr[index], arr[i], temp);
+        temp = arr[index];
+        arr[index] = arr[i];
+        arr[i] = temp;
     }
 
     for(i=0; i<10;i++){
