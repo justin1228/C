5970615be1d36dda9066d0e16b9c701a08406371
justin1228 <didls1228@gmail.com>
2020-05-23 12:26:01 +0900

    tP

--- triangularPattern.c
+++ triangularPattern.c
@@ -4,16 +4,19 @@ int main(){
     int n;
     scanf("%d", &n);
 
-    for(int i = 1; i <= n; i++){
-        for(int j = 1; j <= n; j++){
-            if(j>i)
-            {
-                break;
-            }
-            else
-            {
-                printf("*");
-            }
+    for(int i = 0; i < n; i++){
+        int tmp = n*2-i-1;
+        for(int j = 0; j < n*2; j++){
+            
+           if(j<=i){
+               printf("*");
+           }
+           else if(j>i && j<tmp){
+               printf(" ");
+           }
+           else{
+               printf("*");
+           }
         }
         printf(" \n");
     }
